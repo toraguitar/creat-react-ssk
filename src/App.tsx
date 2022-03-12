67ede1b969dd2c1de568c3cfed33fa683e12bab6
工藤 景虎 <kagetora_kudo@waku-2.com>
2022-03-12 18:34:22 +0900

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit
           <code>src/App.tsx</code>
-          and save to reload.
+          and save to reload.aaa
         </p>
         <a
           className="App-link"
